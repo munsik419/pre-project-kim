d76eb15d9cccb38d05f9e28fd0a66061464a4efe
kimmunsik <munsik419@gmail.com>
2022-08-23 11:20:22 +0900

    ToDo Test Ver.1

---
+++ kimmunsik/src/main/java/preproject/kimmunsik/KimmunsikApplication.java
@@ -0,0 +1,13 @@
+package preproject.kimmunsik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimmunsikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimmunsikApplication.class, args);
+	}
+
+}
